1eee19a0ad9dfd049ac917dce620c23d80c21e7b
Elvan Hidayat <elvanhd@gmail.com>
2023-06-09 07:48:59 +0700

    proper error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 
@@ -26,8 +27,7 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      console.log('Email in use');
-      return res.send({});
+      throw new BadRequestError('Email in use');
     }
 
     const user = User.build({ email, password });
