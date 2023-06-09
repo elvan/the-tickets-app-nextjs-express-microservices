f6200f2d51c96b22845f68b7a87ae72021cce656
Elvan Hidayat <elvanhd@gmail.com>
2023-06-10 01:53:55 +0700

    current-user middleware

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    req.currentUser = payload;
+  } catch (err) {}
+
+  next();
+};
