52c514d69cc7e51e1c986cac49c3d79e843078ee
Elvan Hidayat <elvanhd@gmail.com>
2023-06-09 16:09:24 +0700

    generating a jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
@@ -11,7 +12,15 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
 const app = express();
+
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -27,14 +36,15 @@ app.use(errorHandler);
 const start = async () => {
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
-};
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
-});
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
 
 start();
