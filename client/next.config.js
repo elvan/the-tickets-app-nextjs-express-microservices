6333aa4578fedf1325bdc3a1afbfb2353e6c26d3
Elvan Hidayat <elvanhd@gmail.com>
2023-06-10 17:26:09 +0700

    fix file change detection

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  webpack: (config) => {
+    config.watchOptions.poll = 300;
+    return config;
+  },
 };
-
-module.exports = nextConfig;
