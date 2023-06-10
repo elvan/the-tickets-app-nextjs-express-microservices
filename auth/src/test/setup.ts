69355bc1fac4017e387e47f5811a60ef32744570
Elvan Hidayat <elvanhd@gmail.com>
2023-06-10 12:50:58 +0700

    test environment setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,27 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongo: any;
+
+beforeAll(async () => {
+  const mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+
+  await mongoose.connection.close();
+});
