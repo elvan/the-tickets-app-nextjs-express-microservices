08650d0aa2dac996055a48e81a66874dcb29fccf
Elvan Hidayat <elvanhd@gmail.com>
2023-06-10 19:10:21 +0700

    an onSuccess callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,3 +1,4 @@
+import Router from 'next/router';
 import { useState } from 'react';
 import useRequest from '../../hooks/use-request';
 
@@ -11,12 +12,13 @@ export default () => {
       email,
       password,
     },
+    onSuccess: () => Router.push('/'),
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    doRequest();
+    await doRequest();
   };
 
   return (
