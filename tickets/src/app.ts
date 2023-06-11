63fbade145467b49fba810a710fe88b0326afd93
Elvan Hidayat <elvanhd@gmail.com>
2023-06-11 16:35:40 +0700

    creating the router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,11 +1,11 @@
-import { NotFoundError, errorHandler } from '@the-tickets/common';
+import { errorHandler, NotFoundError } from '@the-tickets/common';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
-
 app.set('trust proxy', true);
 app.use(json());
 app.use(
@@ -14,6 +14,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,4 +1,11 @@
-it('has a route handler listening to /api/tickets for post requests', async () => {});
+import request from 'supertest';
+import { app } from '../../app';
+
+it('has a route handler listening to /api/tickets for post requests', async () => {
+  const response = await request(app).post('/api/tickets').send({});
+
+  expect(response.status).not.toEqual(404);
+});
 
 it('can only be accessed if the user is signed in', async () => {});
 
