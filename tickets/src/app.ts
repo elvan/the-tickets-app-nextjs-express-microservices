8b230dcaa8849dfec353f5b99f34c9a11a7bfe66
Elvan Hidayat <elvanhd@gmail.com>
2023-06-11 23:39:16 +0700

    handling updates

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import 'express-async-errors';
 import { indexTicketRouter } from './routes/index';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -21,6 +22,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
---
+++ tickets/src/routes/update.ts
@@ -0,0 +1,21 @@
+import { NotFoundError, requireAuth } from '@the-tickets/common';
+import express, { Request, Response } from 'express';
+import { Ticket } from '../models/ticket';
+
+const router = express.Router();
+
+router.put(
+  '/api/tickets/:id',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    res.send(ticket);
+  }
+);
+
+export { router as updateTicketRouter };
