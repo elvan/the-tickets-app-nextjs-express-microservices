f0d75fdc0469ef044f684e599dfed4e819c5c317
Elvan Hidayat <elvanhd@gmail.com>
2023-06-11 23:57:54 +0700

    final update changes

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -2,8 +2,10 @@ import {
   NotAuthorizedError,
   NotFoundError,
   requireAuth,
+  validateRequest,
 } from '@the-tickets/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
@@ -11,6 +13,13 @@ const router = express.Router();
 router.put(
   '/api/tickets/:id',
   requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be provided and must be greater than 0'),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
 
@@ -21,6 +30,12 @@ router.put(
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
+
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price,
+    });
+    await ticket.save();
 
     res.send(ticket);
   }
