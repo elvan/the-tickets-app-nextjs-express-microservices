80ccd1d7fc2889c3db1abe391297d595b9d6cad8
Elvan Hidayat <elvanhd@gmail.com>
2023-08-13 21:17:48 +0700

    create a custom publisher

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -5,7 +5,6 @@ import { TicketCreatedEvent } from './ticket-created-event';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
-
   queueGroupName = 'payments-service';
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {
