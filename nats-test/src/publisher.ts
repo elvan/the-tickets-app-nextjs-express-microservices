8e5077cc486d99e352e0ae0374a6b45a9be8147a
Elvan Hidayat <elvanhd@gmail.com>
2023-08-13 21:26:43 +0700

    awaiting event publication

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,15 +7,19 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 
   // const data = JSON.stringify({
   //   id: '123',
