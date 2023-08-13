f8e4bed7877599030c4facaa2fd529f5300f5d61
Elvan Hidayat <elvanhd@gmail.com>
2023-08-14 04:26:35 +0700

    graceful shutdown

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,18 +1,25 @@
 import mongoose from 'mongoose';
 
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
+    await natsWrapper.connect('ticketing', 'alsdkj', 'http://nats-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
