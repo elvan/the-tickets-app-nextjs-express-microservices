a598e11ac75cfb94549ec4bd071c76c14282db39
Elvan Hidayat <elvanhd@gmail.com>
2023-08-13 17:31:17 +0700

    event redelivery

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,12 +15,12 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'orders-service-queue-group',
-    options,
-  );
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable();
+
+  const subscription = stan.subscribe('ticket:created', options);
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
