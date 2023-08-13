e0cec4b3c33e3787f0edd0abe4638867b0826b64
Elvan Hidayat <elvanhd@gmail.com>
2023-08-13 20:51:11 +0700

    enforcing listener subjects

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,17 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { Subjects } from './subjects';
+import { TicketCreatedEvent } from './ticket-created-event';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
   onMessage(data: any, msg: Message) {
     console.log('Event data!', data);
+
+    console.log(data.name);
+    console.log(data.cost);
 
     msg.ack();
   }
