549404d3e3d1ac46fd4bf318dfd43dd795b31abd
Elvan Hidayat <elvanhd@gmail.com>
2023-08-14 19:38:36 +0700

    scaffolding the order route handlers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,10 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +18,11 @@ app.use(
   }),
 );
 app.use(currentUser);
+
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
