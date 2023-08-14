ef135ff6cda9eaf845ca44e58335550d789f7d6c
Elvan Hidayat <elvanhd@gmail.com>
2023-08-14 09:23:12 +0700

    creating the orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,24 @@
+import { NotFoundError, currentUser, errorHandler } from '@the-tickets/common';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import express from 'express';
+import 'express-async-errors';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+app.use(currentUser);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
