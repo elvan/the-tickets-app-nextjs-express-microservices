36f0bf577b58dbb2689cfe03944da8dc1fd26643
Elvan Hidayat <elvanhd@gmail.com>
2023-08-15 21:23:49 +0700

    queue group name constant

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,9 +1,10 @@
 import { Listener, Subjects, TicketCreatedEvent } from '@the-tickets/common';
 import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
 }
