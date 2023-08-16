deca86fc7701b9ab257e2caccc85e9d48d2d4aeb
Elvan Hidayat <elvanhd@gmail.com>
2023-08-16 08:35:13 +0700

    updating tickets event definitions

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -25,11 +25,12 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
-    await new TicketCreatedPublisher(natsWrapper.client).publish({
+    new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
+      version: ticket.version,
     });
 
     res.status(201).send(ticket);
