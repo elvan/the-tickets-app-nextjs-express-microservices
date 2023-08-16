214cd17ff8f4409dbfc1d2b427e1849adadb5943
Elvan Hidayat <elvanhd@gmail.com>
2023-08-16 16:02:58 +0700

    rejecting edits of reserved tickets

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestError,
   NotAuthorizedError,
   NotFoundError,
   requireAuth,
@@ -27,6 +28,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError('Cannot edit a reserved ticket');
     }
 
     if (ticket.userId !== req.currentUser!.id) {
