437de59140e914785f875552708808925976f302
Elvan Hidayat <elvanhd@gmail.com>
2023-08-16 22:08:35 +0700

    defining the expiration complete event

--- common/src/index.ts
+++ common/src/index.ts
@@ -12,6 +12,7 @@ export * from './middlewares/validate-request';
 
 export * from './events/base-listener';
 export * from './events/base-publisher';
+export * from './events/expiration-complete-event';
 export * from './events/order-cancelled-event';
 export * from './events/order-created-event';
 export * from './events/subjects';
