01eda3b39ea114aaf685513bdc5741ff943e4936
Elvan Hidayat <elvanhd@gmail.com>
2023-08-18 00:28:36 +0700

    validating order payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,14 @@
-import { requireAuth, validateRequest } from '@the-tickets/common';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@the-tickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -10,6 +18,20 @@ router.post(
   [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
     res.send({ success: true });
   },
 );
