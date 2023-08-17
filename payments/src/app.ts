a17aefe63b25f9237c3bb089cf8b4f06311f9d43
Elvan Hidayat <elvanhd@gmail.com>
2023-08-18 06:54:47 +0700

    testing the order validation

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -3,6 +3,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
+import { createChargeRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +15,8 @@ app.use(
   }),
 );
 app.use(currentUser);
+
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -35,10 +35,10 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
   // Build a JWT payload.  { id, email }
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 
